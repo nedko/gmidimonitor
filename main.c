f730afdd11e50f1146e8c459f86539cdaa410972
nedko <nedko@d9434dc6-2408-0410-b8be-9e73975a53a3>
2007-03-17 19:19:37 +0000

    Remove printf dumping of jack midi events
    git-svn-id: svn://svn.gna.org/svn/gmidimonitor/trunk@37 d9434dc6-2408-0410-b8be-9e73975a53a3

--- main.c
+++ main.c
@@ -1002,101 +1002,6 @@ alsa_midi_thread(void * context_ptr)
 
 #ifdef HAVE_JACK_MIDI
 
-void
-jack_print_raw(unsigned char* buf, size_t len)
-{
-  size_t i;
-  for (i = 0 ; i < len; i++)
-  {
-    printf("0x%X ", buf[i] & 0xFF);
-  }
-
-  printf("\n");
-}
-
-void
-jack_print_sysex(unsigned char* buf, size_t len)
-{
-  size_t i;
-
-  assert(buf[0] == 0xF0 && buf[1] == 0x7F);
-
-  if (buf[3] == 0x01 && buf[4] == 0x01)
-  {
-    printf("MTC Full Frame (%hhu:%hhu:%hhu:%hhu)\n", buf[5] &0xF, buf[6], buf[7], buf[8]);
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x01)
-  {
-    printf("MMC Stop\n");
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x02)
-  {
-    printf("MMC Play\n");
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x03)
-  {
-    printf("MMC Deferred Play\n");
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x04)
-  {
-    printf("MMC Fast Forward\n");
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x05)
-  {
-    printf("MMC Rewind\n");
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x06)
-  {
-    printf("MMC Record Strobe (Punch In)\n");
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x07)
-  {
-    printf("MMC Record Exit (Punch Out)\n");
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x09)
-  {
-    printf("MMC Pause\n");
-  }
-  else if (buf[3] == 0x06 && buf[4] == 0x44 && buf[5] == 0x06 && buf[6] == 0x01)
-  {
-    printf("MMC Goto (%hhu:%hhu:%hhu:%hhu:%hhu)\n", buf[7], buf[8], buf[9], buf[10], buf[11]);
-  }
-  else
-  {
-    printf("Unknown sysex: ");
-    for (i = 0; i < len; i++)
-    {
-      printf("0x%X ", buf[i] & 0xFF);
-
-      if (buf[i] == 0xF7)
-      {
-        break;
-      }
-    }
-
-    printf("\n");
-  }
-}
-
-
-void
-jack_print_midi_message(unsigned char* buf, size_t len)
-{ 
-  if (buf[0] == 0xF0 && buf[1] == 0x7F)
-  {
-    jack_print_sysex(buf, len);
-  }
-  else if (buf[0] == 0xF1)
-  {
-    printf("MTC Quarter Frame %d\n", (buf[1] & 0xF0) >> 4);
-  }
-  else
-  {
-    printf("Unknown Raw: ");
-    jack_print_raw(buf, len);
-  }
-}
-
 int
 jack_process(jack_nframes_t nframes, void * context)
 {
@@ -1114,7 +1019,11 @@ jack_process(jack_nframes_t nframes, void * context)
   {
     jack_midi_event_get(&in_event, port_buf, i, nframes);
 
-    jack_print_midi_message(in_event.buffer, in_event.size);
+    /* allocate memory for buffer copy */
+
+    /* copy buffer data */
+
+    //jack_process_midi_message(in_event.buffer, in_event.size);
   }
 
   return 0;
