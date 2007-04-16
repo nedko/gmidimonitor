755dfb7801c0235a8ea788491c14bd1dabd7846d
nedko <nedko@d9434dc6-2408-0410-b8be-9e73975a53a3>
2007-04-16 13:40:15 +0000

    improve jack midi variants compatibility
    git-svn-id: svn://svn.gna.org/svn/gmidimonitor/trunk@68 d9434dc6-2408-0410-b8be-9e73975a53a3

--- jack_compat.c
+++ jack_compat.c
@@ -20,18 +20,20 @@
  *
  *****************************************************************************/
 
-#if defined(HAVE_OLD_JACK_MIDI)
-#define jack_midi_get_event_count(port_buf, nframes) jack_midi_port_get_info(port_buf, nframes)->event_count
-#endif
-
 #if defined(JACK_MIDI_NEEDS_NFRAMES)
 
 jack_nframes_t
 jack_midi_get_event_count_compat(
   void * port_buffer)
 {
+#if defined(HAVE_OLD_JACK_MIDI)
+  return jack_midi_port_get_info(port_buffer, 0)->event_count;
+#else
   return jack_midi_get_event_count(port_buffer, 0);
+#endif
 }
+
+#define jack_midi_get_event_count jack_midi_get_event_count_compat
 
 int
 jack_midi_event_get_compat(
@@ -42,7 +44,12 @@ jack_midi_event_get_compat(
   return jack_midi_event_get(event, port_buffer, event_index, 0);
 }
 
-#define jack_midi_get_event_count jack_midi_get_event_count_compat
 #define jack_midi_event_get jack_midi_event_get_compat
 
-#endif  /* #if defined(HAVE_JACK_MIDI_WITH_NFRAMES) */
+#else
+
+#if defined(HAVE_OLD_JACK_MIDI)
+#error "Old (0.102.20) JACK MIDI API needs nframes (autotools probably gone mad)"
+#endif
+
+#endif  /* #if defined(JACK_MIDI_NEEDS_NFRAMES) */
